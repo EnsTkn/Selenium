d32db01d5f339cae02d2a203e7db704f94139c41
EnsTkn <ente4343@gmail.com>
2019-11-10 23:27:51 -0500

    hello

--- src/test/java/com/cbt/tests/TestCase8.java
+++ src/test/java/com/cbt/tests/TestCase8.java
@@ -1,4 +1,25 @@
 package com.cbt.tests;
 
+import com.cbt.utilities.BrowserFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class TestCase8 {
+    public static void main(String[] args) {
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+        driver.get("https://practice-cybertekschool.herokuapp.com/");
+
+        driver.findElement(By.xpath("//a[.='Registration Form']")).click();
+
+        driver.findElement(By.name("phone")).sendKeys("5711234354”");
+
+        String a = driver.findElement(By.xpath("//small[@data-bv-validator='regexp' and @data-bv-for='phone']")).getText();
+
+        String b = "Phone format is not correct";
+
+        System.out.println((a.equals(b) ? "passed" : "failed"));
+
+
+        driver.quit();
+    }
 }
